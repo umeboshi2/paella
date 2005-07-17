38170bb8a2928eebcb46e0e07e2f2d81c23e4fdc
umeboshi <umeboshi@cfc4e7be-4be4-0310-bcfe-fc894edce94f>
2005-07-17 00:45:25 +0000

    removing directories to begin moving unstable over

--- src/paella/test/xml_client.py
+++
@@ -1,8 +0,0 @@
-from xmlrpclib import ServerProxy, SafeTransport
-
-from paella.base import debug, Error
-from paella.debian.base import RepositorySource, islocaluri
-
-
-
-tc = ServerProxy('http://premio:8000', None, None, 0, allow_none=True)
